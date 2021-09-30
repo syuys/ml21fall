2154139b009de7b215188b51444498eb58d6207b
eric <cure845612345@gmail.com>
2021-09-30 15:42:20 +0800

    Add features.

--- PLA_algorithm.py
+++ PLA_algorithm.py
@@ -32,12 +32,12 @@ def getY(w, x):
 start = -10
 end = 10
 num = 20
-x1s = np.random.uniform(start+0.5, end-0.5, num)
-x2s = np.random.uniform(start+0.5, end-0.5, num)
+x1s = np.random.uniform(start, end, num)
+x2s = np.random.uniform(start, end, num)
 xSet = np.concatenate((x1s[None, :], x2s[None, :]), axis=0)
 
 # %% set f vector and label the points
-f = np.array([5, -2])
+f = np.array([2, -2])
 ySet = np.sign(getY(f, xSet))
 
 plt.axes().set_aspect("equal")
@@ -45,36 +45,27 @@ plt.axes().set_aspect("equal")
 plt.plot(x1s[np.where(ySet<0)], x2s[np.where(ySet<0)], "x", color="black")
 plt.plot(x1s[np.where(ySet>0)], x2s[np.where(ySet>0)], "o", color="red")
 # plot f
-lineX1 = np.linspace(start, end, 100)
+lineX1 = np.linspace(start*1.5, end*1.5, 100)
 lineX2 = getX2(f, lineX1)
 plt.plot(lineX1, lineX2, color="blue")
-plt.xlim(start, end)
-plt.ylim(start, end)
+plt.xlim(start*1.5, end*1.5)
+plt.ylim(start*1.5, end*1.5)
 plt.show()
 
 # %% find g iteratively
-g = np.array([np.random.uniform(0, 1), np.random.uniform(-1, 1)])
+g = np.array([np.random.uniform(-1, 1), np.random.uniform(-1, 1)])
+print("initial g:", g)
 
 for _ in range(100):
-    yprimeSet = np.sign(getY(g, xSet))
-    plt.axes().set_aspect("equal")
-    # plot original points
-    plt.plot(x1s[np.where(ySet<0)], x2s[np.where(ySet<0)], "x", color="black")
-    plt.plot(x1s[np.where(ySet>0)], x2s[np.where(ySet>0)], "o", color="red")
-    # make first coefficient of g be positive
-    if g[0] < 0:
-        g = -g
-    # plot g
-    lineX1 = np.linspace(start, end, 100)
-    lineX2 = getX2(g, lineX1)
-    plt.plot(lineX1, lineX2, "b--")
-    # plot error points
-    plt.scatter(x1s[np.where(ySet!=yprimeSet)], x2s[np.where(ySet!=yprimeSet)], s=200, color="none", edgecolor="blue")
-    plt.xlim(start, end)
-    plt.ylim(start, end)
-    plt.show()
+    # add new g to the plot
+    if _ > 0:
+        plt.arrow(0, 0, g[0], g[1], head_width=0.5, width=0.1, linestyle="--")
+        plt.show()
     
-    # choose the nearst error point
+    # calculate the guessed yprime
+    yprimeSet = np.sign(getY(g, xSet))
+    
+    # catch the nearst error point
     if np.where(ySet!=yprimeSet)[0].size == 0:
         print("final g:", g)
         break
@@ -87,6 +78,41 @@ for _ in range(100):
     x2sErrorChosen = x2sError[np.argmin(distanceSet)]
     ySetErrorChosen = ySetError[np.argmin(distanceSet)]
     
+    # plot
+    plt.axes().set_aspect("equal")
+    # plot original points
+    plt.plot(x1s[np.where(ySet<0)], x2s[np.where(ySet<0)], "x", color="black")
+    plt.plot(x1s[np.where(ySet>0)], x2s[np.where(ySet>0)], "o", color="red")
+    # make first coefficient of g be positive
+    # if g[0] < 0:
+    #     g = -g
+    # mark error points
+    plt.scatter(x1s[np.where(ySet!=yprimeSet)], x2s[np.where(ySet!=yprimeSet)], s=200, color="none", edgecolor="blue")
+    # highlight the nearest error point
+    plt.scatter(x1sErrorChosen, x2sErrorChosen, s=200, color="none", edgecolor="blue", linewidths=3)
+    # mark g and normal vector
+    lineX1 = np.linspace(start*1.5, end*1.5, 100)
+    lineX2 = getX2(g, lineX1)
+    plt.plot(lineX1, lineX2, "b--")
+    plt.arrow(0, 0, g[0], g[1], head_width=0.5, width=0.1)
+    plt.xlim(start*1.5, end*1.5)
+    plt.ylim(start*1.5, end*1.5)
+    plt.title("iter. {}, g: {}".format(_, np.round(g, 2)))
+    
     # update g
     g = g + np.sign(ySetErrorChosen) * np.array([x1sErrorChosen, x2sErrorChosen])
     
+# final plot
+plt.axes().set_aspect("equal")
+# plot original points
+plt.plot(x1s[np.where(ySet<0)], x2s[np.where(ySet<0)], "x", color="black")
+plt.plot(x1s[np.where(ySet>0)], x2s[np.where(ySet>0)], "o", color="red")
+# mark g and normal vector
+lineX1 = np.linspace(start*1.5, end*1.5, 100)
+lineX2 = getX2(g, lineX1)
+plt.arrow(0, 0, g[0], g[1], head_width=0.5, width=0.1)
+plt.plot(lineX1, lineX2, "b--")
+plt.xlim(start*1.5, end*1.5)
+plt.ylim(start*1.5, end*1.5)
+plt.title("iter. {} --- Final.".format(_))
+plt.show()
