a93c72b118382a0b99a7be73882ca262b87af4f4
eric <cure845612345@gmail.com>
2021-10-20 15:48:42 +0800

    Add description and set different seeds.

--- hw1/Q13.py
+++ hw1/Q13.py
@@ -4,17 +4,22 @@
 Created on Sat Oct 16 05:37:08 2021
 
 @author: YI SIANG SYU
+
+Description:
+    There are four main parts in this .py file. First is to import necessary packages. 
+    Second is to define function I used in PLA algorithm. Third is to load data and do
+    some preprocessing like adding x0 to every xn. Fourth is the key part for doing
+    PLA loop (each experiment is done with different random seed).
+    For this question, there is no scaling factor.
 """
 
+
+# %% import necessary packages
 from IPython import get_ipython
 get_ipython().magic('clear')
 get_ipython().magic('reset -f')
-import matplotlib.pyplot as plt
-plt.close("all")
 import numpy as np
-plt.rcParams.update({"mathtext.default": "regular"})
-plt.rcParams["font.family"] = "Times New Roman"
-plt.rcParams["figure.dpi"] = 300
+from datetime import datetime
 
 
 # %% define function
@@ -31,8 +36,7 @@ def getH(w, x):
     return h    
 
 
-# %% 
-
+# %% preprocess
 # load data
 dataSet = np.genfromtxt("hw1_train.dat")
 
@@ -45,32 +49,31 @@ ySet = dataSet[:, -1]
 # set other related parameters
 cumuCorrectThold = 5*dataSet.shape[0]
 repeatTestTimes = 1000
-wSet = np.empty((repeatTestTimes, xSet.shape[1]))
-
-
-# %% preprocess
-
+wSet = np.empty((repeatTestTimes, xSet.shape[1]))  # container for storing wPLA
 
 
 # %% main - do experiment
-for testIdx in range(1000):
+currentTime = int(datetime.now().strftime("%H:%M:%S").replace(":", ""))  # get current time, used for seed setting.
+print("Start to do PLA!")
+print("\nMonitor progress:")
+for testIdx in range(repeatTestTimes):  # Repeat experiment for 1000 times
     # initialization
     t = 0
     cumuCorrect = 0
-    w = np.zeros(xSet.shape[1])  # initial w
+    w = np.zeros(xSet.shape[1])  # initialize w
+    np.random.seed(int(currentTime*1e4)+testIdx)  # ensure each experiment is done with different seed.
     # do PLA
     while(True):
-        idx = np.random.randint(0, xSet.shape[0])
+        idx = np.random.randint(0, xSet.shape[0])  # randomly picks an example in every iteration (with replacement)
         if getH(w, xSet[idx]) != ySet[idx]:
             cumuCorrect = 0
-            w = w + ySet[idx]*xSet[idx]
+            w = w + ySet[idx]*xSet[idx]  # updates w if and only if w is incorrect on the example.
         else:
             cumuCorrect += 1
             if cumuCorrect >= cumuCorrectThold:
-                break
+                break  # stop updating and return w as wPLA if w is correct consecutively after checking 5N randomly-picked examples.
         t += 1
     wSet[testIdx] = w
     print("{}, ".format(testIdx), end="")
-print("\n\n")
-print("Average squared length of wPLA:", (wSet**2).sum(axis=1).mean())
-
+print("\n\nDone!")
+print("Average squared length of wPLA:", (wSet**2).sum(axis=1).mean())  # show the average squared length of wPLA
